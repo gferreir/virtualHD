258eed781acc498dd2df8be4c39b626f91211529
Caioww <sousinhacaio@gmail.com>
2019-09-22 14:06:58 -0300

    separando criacao do hd em funcao

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
-#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 using namespace std;
+
+void criaHD(char str[100] ,int i){
+	int j=0;
+	char HD[100];
+	while(str[i] !='\0'){
+		HD[j]=str[i];
+		
+		if(str[i + 1] == '\0'){
+			HD[j+1] = str[i+1];
+		}
+		i++;
+		j++;
+			
+	}
+	
+	cout << "# " << HD << "> ";
+	
+	strcat(HD, ".txt");
+	FILE *file = fopen(HD, "wt");
+		fclose(file);
+
+	
+	
+}
+
 
 
 
@@ -23,58 +51,26 @@ int main(int argc, char *argv[])
 	int i =0, j = 0;
 
 	while(str[i] == createHd[i] && str[i] !='\0'){
-		i++;
 		if(str[i] != createHd[i]){
 			printf("Errado");
 			return 0;
+			
 		}
+		i++;
 	}
 	
 	
 	
 	if(i==8){
 		i++;
-		while(str[i] !='\0'){
-			HD[j]=str[i];
-			
-			if(str[i + 1] == '\0'){
-				HD[j+1] = str[i+1];
-			}
-			i++;
-			j++;
-			
-		}
-		i=0;
-		j=0;
+		criaHD(str, i);
+	
+	}else{
+		cout<< "Não foi possivel criar o HD";
 	}
 	
-	cout << "# " << HD << "> ";
-	gets(arquivo);
 
-	while(arquivo[i] == create[i] && arquivo[i] !='\0'){
-		i++;
-	}
-
-	if(i==8){
-		i++;
-		while(arquivo[i] !='\0'){
-			nome[j]=arquivo[i];
-
-			if(arquivo[i + 1] == '\0'){
-				nome[j+1] = arquivo[i+1];
-			}
-			i++;
-			j++;
-
-		}
-		i=0;
-		j=0;
-	}
-
-
-	ofstream file;
-	file.open(HD);
-	file.close();
+	
 
 	system("pause");
 	return 0;
